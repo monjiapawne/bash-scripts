ccbe088de1d0de2d7d7feda8c9a5a8e813b0ec48
pan <pan@admin-ws01.incanet.lan>
2025-05-22 22:01:33 -0400

    Rename gr.sh to gcc-compile-run.sh

---
+++ gcc-compile-run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# gr = gcc run, compiles with gcc and runs locally.
+file="$1"
+fullpath="$PWD/$file"
+filetype=".c"
+
+function main() {
+    if [[ $file == *$filetype && -f $fullpath ]]; then
+        if command -v gcc >/dev/null 2>&1; then
+            compiledFile=${file%.*}
+            gcc "$fullpath" -o $compiledFile
+            ./$compiledFile
+        else
+            echo 'gcc is not installed'
+        fi
+    else
+        echo 'verify file format and location.'
+        echo "file: $fullpath"
+    fi
+}
+
+main
